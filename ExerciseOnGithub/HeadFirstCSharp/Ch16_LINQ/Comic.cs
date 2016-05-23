8f423c3f42ea4db3aeabff63a5067f494468f80e
Rudolf <chachen0117@gmail.com>
2016-05-23 23:13:41 +0800

    add HeadFirstCSharp

---
+++ ExerciseOnGithub/HeadFirstCSharp/Ch16_LINQ/Comic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp.Ch16_LINQ
+{
+    class Comic
+    {
+        public string Name { get; set; }
+        public int Issue { get; set; }
+
+        public Comic(String Name, int Issue)
+        {
+            this.Name = Name;
+            this.Issue = Issue;
+        }
+
+        private static List<Comic> BuildCatalog(){
+            List<Comic> comics = new List<Comic>();
+
+            comics.Add(new Comic("Johnny America vs. the Pinko", 6));
+            comics.Add(new Comic("Rock and Roll (limited edition)", 19));
+            comics.Add(new Comic("Woman's Work", 36));
+            comics.Add(new Comic("Hippie Madness (misprinted)", 57));
+            comics.Add(new Comic("Revenge of the New Wave Freak (damaged)", 68));
+            comics.Add(new Comic("Black Monday", 74));
+            comics.Add(new Comic("Tribal Tattoo Madness", 83));
+            comics.Add(new Comic("The Death of an Object", 97));
+            return comics;
+        }
+
+        private static Dictionary<int,int> GetPrices()
+        {
+            Dictionary<int, int> values = new Dictionary<int, int> ();
+            values.Add(6, 3600);
+            values.Add(19, 500);
+            values.Add(36, 650);
+            values.Add(57, 13525);
+            values.Add(68, 250);
+            values.Add(74, 75);
+            values.Add(83, 25);
+            values.Add(97, 35);
+            return values;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Comic> comics = BuildCatalog();
+            Dictionary<int, int> values = GetPrices();
+
+            var mostExpensive = from comic in comics
+                                where values[comic.Issue] > 500
+                                orderby values[comic.Issue] descending
+                                select comic;
+
+            foreach (Comic comic in mostExpensive)
+                Console.WriteLine("{0} is worth {1:c}", comic.Name, values[comic.Issue]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExerciseOnGithub/HeadFirstCSharp/Ch16_LINQ/P_658.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstCSharp.Ch16_LINQ
+{
+    class P_658
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+        //    var result = from v in values
+        //                 where v < 37
+        //                 orderby v
+        //                 select v;
+
+        //    foreach (int i in result)
+        //        Console.WriteLine(i);
+
+        //    Console.ReadLine();
+        //}
+        
+    }
+}
